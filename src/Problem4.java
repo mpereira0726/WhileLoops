d84f4b1593f22dd91fd574a09a459d89278357a1
mpereira0726 <106694216+mpereira0726@users.noreply.github.com>
2022-06-20 13:54:59 -0400

    second commit

--- src/Problem4.java
+++ src/Problem4.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Problem4 {
+import java.util.Scanner;
+
+public class Problem4 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner (System.in);
+        System.out.print("Enter integer n: ");
+        int n = input.nextInt();
+
+        int sum = 0;
+        int num;
+
+        while (n > 0) {
+            num = n % 10;
+            sum += num;
+            n/= 10;
+        }
+        System.out.print("The sum of all digits of " + n + " is: " + sum);
+    }
 }
