696f5f43dee0f9bee9719715311639a5894bd7e0
mpereira0726 <106694216+mpereira0726@users.noreply.github.com>
2022-06-20 13:36:34 -0400

    Array Project

---
+++ src/Array.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Array {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter an integer between 1 and 20");
+
+        int n = input.nextInt();
+
+        if (n >= 1 && n <= 20) {
+
+            int arr[] = new int[n];
+
+            for(int i = 0; i < arr.length; i++)
+        {
+            arr[i] = (int)Math.pow(2,i);
+            System.out.println(arr [i]);
+        }}
+    }
+}
